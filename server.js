7c4e86b6715dc72ec104de88a66d51627b30c6d8
Jamil Kassem <jamilahkassem@gmail.com>
2026-05-03 11:40:30 +0200

    Added question logic

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
 const { setTimeout: sleep } = require('timers/promises');
 
-let questionId = 0;
+let question_id = 0;
+let question_image = 0;
+let question_number_answers = 0;
 let active = false;
-let counter = 1;
 const portSocket = 9002;
 
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: portSocket });
-let phase = 0, NumberOfUsers = 0, NAdmins = 0,Requests=0;
+let phase = 0, NumberOfUsers = 0, NAdmins = 0, Requests = 0;
 const phases = ["No Admin", "Admin connected", "Started", "Finished"]
 
 async function EndQuestion(time)
 {
     await sleep(time*100);
     active = false;
-    broadcast({type:"Pause",a:counter});
+    broadcast({type:"Pause"});
 }
-
 
 wss.on('connection', (socket) =>
 {
     Requests++;
     NumberOfUsers++;
     WD();
-    if(active){socket.send(JSON.stringify({type:"Next", a:questionId}));}else{socket.send(JSON.stringify({type:"Pause",a:counter}));}
-    socket.on('message', (data)=>{broadcast(JSON.parse(data.toString()));});
-    socket.on('close',()=>{NumberOfUsers--;WD();});
-    socket.on('error', (err)=>{console.error("Socket error:", err);});
+    if (active) {
+        socket.send(JSON.stringify({
+            type: "Next",
+            question_id: question_id,
+            question_image: question_image,
+            question_number_answers: question_number_answers
+        }));
+    } else {
+        socket.send(JSON.stringify({type: "Pause"}));
+    }
+    socket.on('close', () => {
+        NumberOfUsers--;
+        WD();
+    });
+    socket.on('error', (err) => {
+        console.error("Socket error:", err);
+    });
+});
+wss.on('NextQuestion', (data) =>
+{
+    let parsed_data = JSON.parse(data.toString());
+    active = true;
+    question_id = parsed_data.question_id;
+    question_image = parsed_data.question_image;
+    question_number_answers = parsed_data.question_number_answers;
+    broadcast({
+        type: "Next",
+        id: question_id,
+        image: question_image,
+        time: parsed_data.time,
+        number_answers: question_number_answers
+    });
+    EndQuestion(parseInt(parsed_data.time)).catch(err => {console.error("EndQuestion error:", err);});
 });
 const broadcast = (message) => {
     wss.clients.forEach((client) => {
